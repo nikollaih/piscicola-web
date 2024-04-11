d799ccc6c3c861ebb7366eefd90aa1c58c1f67c6
Nikollai Hernandez G. <nikollaihernandez@gmail.com>
2024-04-11 13:17:17 -0500

    MQTT fixes

--- app/Console/Commands/ListenMqttCommand.php
+++ app/Console/Commands/ListenMqttCommand.php
@@ -1,11 +1,11 @@
 <?php
 
-
 namespace App\Console\Commands;
 
 use App\Http\Controllers\MqttController;
 use App\Models\AuditorMqttConnection;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use PhpMqtt\Client\Facades\MQTT;
 
 class ListenMqttCommand extends Command
@@ -14,66 +14,51 @@ class ListenMqttCommand extends Command
     protected $signature = 'mqtt:listen';
     protected $description = 'Listen to MQTT broker';
     private MqttController $MqttController;
-    private ?\PhpMqtt\Client\Contracts\MqttClient $mqtt = null;
-    private string $lockFilePath;
 
     public function __construct()
     {
         parent::__construct();
 
         $this->MqttController = new MqttController();
-        $this->lockFilePath = storage_path('app/mqtt-listener.lock');
     }
 
     public function handle()
     {
-        if (!file_exists($this->lockFilePath)) {
-            // Acquire lock
-            touch($this->lockFilePath);
+        $mqttConnectionFile = storage_path('app/mqtt_connection.json');
 
-            try {
-                $this->mqtt = MQTT::connection();
+        // Check if there is an existing MQTT connection
+        if (File::exists($mqttConnectionFile)) {
+            // If there is an existing MQTT connection, close it
+            $previousConnectionId = json_decode(File::get($mqttConnectionFile), true);
+            MQTT::disconnect($previousConnectionId);
+        }
 
-                echo sprintf('Conectado');
-                AuditorMqttConnection::create([
-                    "register_time" => date('Y-m-d H:i:s'),
-                    "status" => 1
-                ]);
+        try {
+            echo sprintf('Conectado');
+            AuditorMqttConnection::create(["register_time" => date('Y-m-d H:i:s'), "status" => 1]);
 
-                $this->mqtt->subscribe(env('MQTT_TURN_ACTUATOR'), function (string $topic, string $message) {
-                    print_r("Actuador");
-                    $this->MqttController->getTurnActuator($topic, $message);
-                }, 1);
+            $mqtt = MQTT::connection();
 
-                $this->mqtt->subscribe(env('MQTT_GET_READINGS'), function (string $topic, string $message) {
-                    print_r("Lectura");
-                    $this->MqttController->setReadings($message);
-                }, 1);
+            $connectionId = $mqtt->getClientId();
 
-                $this->mqtt->loop(true);
+            // Save the ID of the current MQTT connection
+            File::put($mqttConnectionFile, json_encode($connectionId));
 
-            } catch (\Exception $e) {
-                echo sprintf('No es posible conectar: %s', $e->getMessage());
-                AuditorMqttConnection::create([
-                    "register_time" => date('Y-m-d H:i:s'),
-                    "status" => 0,
-                    "failed" => $e->getMessage()
-                ]);
-            } finally {
-                // Release lock
-                if (file_exists($this->lockFilePath)) {
-                    unlink($this->lockFilePath);
-                }
-            }
-        } else {
-            // Another instance of MQTT listener is already running.
-            echo "Another instance of MQTT listener is already running.\n";
-            AuditorMqttConnection::create([
-                "register_time" => date('Y-m-d H:i:s'),
-                "status" => 0,
-                "failed" => "Another instance of MQTT listener is already running."
-            ]);
-            return Command::FAILURE;
+            $mqtt->subscribe(env('MQTT_TURN_ACTUATOR'), function (string $topic, string $message) {
+                print_r("Actuador");
+                $this->MqttController->getTurnActuator($topic, $message);
+            }, 1);
+
+            $mqtt->subscribe(env('MQTT_GET_READINGS'), function (string $topic, string $message) {
+                print_r("Lectura");
+                $this->MqttController->setReadings($message);
+            }, 1);
+
+            $mqtt->loop(true);
+
+        } catch (\Exception $e) {
+            echo sprintf('No es posible conectar'. $e->getMessage());
+            AuditorMqttConnection::create(["register_time" => date('Y-m-d H:i:s'), "status" => 0, "failed" => $e->getMessage()]);
         }
     }
 }
