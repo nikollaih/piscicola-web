24851ed39deaa7aeb471ad9bc83351ea3eccfe96
Nikollai Hernandez G. <nikollaihernandez@gmail.com>
2025-06-20 13:22:16 -0500

    Responsive fixes 2

--- resources/js/Pages/Sowings/Index.jsx
+++ resources/js/Pages/Sowings/Index.jsx
@@ -38,7 +38,7 @@ export default function Sowings({ auth, sowings, request, createSowingUrl, baseU
                             </PrimaryButton>
                         </Link>
                     </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                         {getSowingsDom()}
                     </div>
                     <Pagination className="mt-6" links={sowings.links} search={request.search} />
