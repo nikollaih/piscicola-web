24e53a3aba919ae0f73340d7470257e0d0bb0f5f
Nikollai Hernandez G. <nikollaihernandez@gmail.com>
2025-07-11 15:37:50 -0500

    Remove dashboard notification

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,35 +17,8 @@ class DashboardController extends Controller
 {
     public function index()
     {
-
-        $this->enviarNotificacion();
         return \inertia('Dashboard', [
             'csrfToken' => csrf_token()
         ]);
-    }
-
-    public function enviarNotificacion(): void
-    {
-        $expoPush = app(ExpoPushNotificationService::class);
-
-        $notifications = [
-            [
-                'to' => 'ExponentPushToken[3tGKLyEdjFNqlvgxIuUZBM]',
-                'title' => 'Hola usuario!',
-                'body' => 'Tienes una nueva actualización.',
-                'sound' => 'default',
-                'data' => ['tipo' => 'mensaje', 'id' => 123]
-            ],
-            [
-                'to' => 'ExponentPushToken[ledRe9Eq9gUwgzCkInQ5YI]',
-                'title' => 'Hola usuario!',
-                'body' => 'Tienes una nueva actualización.',
-                'sound' => 'default',
-                'data' => ['tipo' => 'mensaje', 'id' => 123]
-            ]
-        ];
-
-        $result = $expoPush->send($notifications);
-
     }
 }
