6bd0f8e90b9409a69b78501e9d715f94b1d65c3f
Nikollai Hernandez G. <nikollaihernandez@gmail.com>
2025-10-09 12:08:02 -0500

    Dashboard 2

--- resources/js/Components/Dashboard/ActuatorItem.jsx
+++ resources/js/Components/Dashboard/ActuatorItem.jsx
@@ -1,11 +1,27 @@
+import Bomba from "../../../images/bomba_agua.png";
+import Blower from "../../../images/blower.png";
+import {useMemo} from "react";
+
 export const DashboardActuatorStatus = ({actuator}) => {
-    return <div className={'bg-white p-4 rounded-lg flex justify-between items-center'}>
-        <div>
-            <p>{actuator.name}</p>
-            <p className={'text-gray-500 text-sm'}>{actuator?.pond?.name}</p>
-        </div>
-        <div className={'flex flex-col justify-center items-center'}>
-            <div className={`h-[20px] w-[20px] rounded-full   ${(actuator?.is_turned_on === 1) ? 'bg-green-500' : 'bg-red-500'}`} />
+    const getSource = useMemo(() => {
+        if(actuator?.actuator_type?.name === 'Motobomba') {
+            return <img src={Bomba} alt="" className={'max-w-32 mx-auto mix-blend-multiply'}/>
+        }
+
+        return <img src={Blower} alt="" className={'max-w-24 mx-auto mix-blend-multiply py-4'}/>
+    }, [actuator])
+
+    return <div className={'bg-white px-4 pb-4 rounded-lg '}>
+        {getSource}
+        <div className={'rounded-lg flex justify-between items-center'}>
+            <div>
+                <p>{actuator.name}</p>
+                <p className={'text-gray-500 text-sm'}>{actuator?.pond?.name}</p>
+            </div>
+            <div className={'flex flex-col justify-center items-center'}>
+                <div
+                    className={`h-[20px] w-[20px] rounded-full   ${(actuator?.is_turned_on === 1) ? 'bg-green-500' : 'bg-red-500'}`}/>
+            </div>
         </div>
     </div>
 }
--- resources/js/Components/Dashboard/PondItem.jsx
+++ resources/js/Components/Dashboard/PondItem.jsx
@@ -1,8 +1,14 @@
+import Estanque from '@/../images/estanque.png'
+
 export const DashboardPondStatus = ({pond}) => {
-    return <div className={'bg-white p-4 rounded-lg flex justify-between items-center'}>
-        <p>{pond.name}</p>
-        <div className={'flex flex-col justify-center items-center'}>
-            <div className={`h-[20px] w-[20px] rounded-full   ${(pond?.latest_status?.status === 1) ? 'bg-green-500' : 'bg-red-500'}`} />
+    return <div className={'bg-white px-4 pb-4 rounded-lg '}>
+        <img src={Estanque} alt="" className={'max-w-40 mx-auto'}/>
+        <div className={'flex justify-between items-center'}>
+            <p>{pond.name}</p>
+            <div className={'flex flex-col justify-center items-center'}>
+                <div
+                    className={`h-[20px] w-[20px] rounded-full   ${(pond?.latest_status?.status === 1) ? 'bg-green-500' : 'bg-red-500'}`}/>
+            </div>
         </div>
     </div>
 }
